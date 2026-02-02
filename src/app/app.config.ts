813d52d338b663865e17f78421979bfdaf3601a8
Todd <twilliamsark@gmail.com>
2026-02-01 21:14:49 -0600

    initial firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,31 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (environment.useEmulators) {
+        connectAuthEmulator(auth, 'http://localhost:9099');
+      }
+      return auth;
+    }),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (environment.useEmulators) {
+        connectFirestoreEmulator(firestore, 'localhost', 8080);
+      }
+      return firestore;
+    }),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  useEmulators: false, // true == locally
+  firebase: {
+    apiKey: 'AIzaSyAeEMQta87wGqhMdUNndnfeS4UEnQK7-O0',
+    authDomain: 'dojovideolistings.firebaseapp.com',
+    projectId: 'dojovideolistings',
+    storageBucket: 'dojovideolistings.firebasestorage.app',
+    messagingSenderId: '836458113286',
+    appId: '1:836458113286:web:34af3bb0771e069299d687',
+    measurementId: 'G-SN20BPKVD9',
+  },
+};
